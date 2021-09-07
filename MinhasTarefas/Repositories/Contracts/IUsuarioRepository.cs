e9832a4500909a7ec80af18bba418e6eef44704e
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-09-07 20:59:38 -0300

    49. [Estudo] UsuarioRepository

---
+++ MinhasTarefas/Repositories/Contracts/IUsuarioRepository.cs
@@ -0,0 +1,10 @@
+﻿using MinhasTarefas.Models;
+
+namespace MinhasTarefas.Repositories.Contracts
+{
+    public interface IUsuarioRepository
+    {
+        void Cadastrar(ApplicationUser usuario, string senha);
+        ApplicationUser Obter(string email, string senha);
+    }
+}
