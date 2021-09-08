a208c52d4ae750b9eaa12551623190cff3d6497f
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-09-07 21:10:44 -0300

    50. [Estudo] TarefaRepository - Restauração - Parte 1

---
+++ MinhasTarefas/Repositories/Contracts/ITarefaRepository.cs
@@ -0,0 +1,12 @@
+﻿using MinhasTarefas.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MinhasTarefas.Repositories.Contracts
+{
+    public interface ITarefaRepository
+    {
+        void Sincronizacao(List<Tarefa> tarefas);
+        List<Tarefa> Restauracao(ApplicationUser usuario, DateTime dataUltimaSincronizacao);
+    }
+}
--- MinhasTarefas/Repositories/UsuarioRepository.cs
+++ MinhasTarefas/Repositories/UsuarioRepository.cs
@@ -2,10 +2,7 @@
 using MinhasTarefas.Models;
 using MinhasTarefas.Repositories.Contracts;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MinhasTarefas.Repositories
 {
